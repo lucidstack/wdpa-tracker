5f078f6e255cbff254496a889ca8f7be7f3ad3c2
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-05-11 11:40:48 +0100

    add WdpaPhoenix.ProtectedAreaView.country_name helper

--- apps/wdpa_phoenix/web/views/protected_area_view.ex
+++ apps/wdpa_phoenix/web/views/protected_area_view.ex
@@ -1,3 +1,6 @@
 defmodule WdpaPhoenix.ProtectedAreaView do
   use WdpaPhoenix.Web, :view
+
+  def country_name(%{country: nil}),     do: ""
+  def country_name(%{country: country}), do: country.name
 end
